55d0e6769038b192dea17532ce90b40aa0060652
Nataliia-Kuchuk <natkuchuk@gmail.com>
2022-09-19 20:13:10 +0300

    create header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
+      <Footer/>
     </div>
   );
 }
