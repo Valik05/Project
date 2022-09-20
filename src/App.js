9ff9f08d336c77de1ba3bd75a3e17b18b27a4169
Valik05 <chinchik38@gmail.com>
2022-09-20 15:24:05 +0300

    create slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from './Components/Footer'
 import Header from './Components/Header'
+import Main from './Page/Main'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   )
